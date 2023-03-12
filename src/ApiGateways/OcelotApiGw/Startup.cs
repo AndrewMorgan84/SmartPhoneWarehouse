43f943bcc484fe29ab7a240396a07dbf13efc1f9
Andrew <andrew.morgan@e.org>
2023-03-12 12:43:06 +0000

    Added Ocelot gateway local configuration

--- src/ApiGateways/OcelotApiGw/Startup.cs
+++ src/ApiGateways/OcelotApiGw/Startup.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
